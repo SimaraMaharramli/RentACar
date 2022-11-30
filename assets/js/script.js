7c7cd84442e46e20226f37b83287993c77d7e2b4
zeyno12 <zeynabbagirova1@gmail.com>
2022-11-30 16:20:56 +0400

    finished section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,27 @@
-const navbar = document.getElementsByClassName('fa-bars')[0]
-const overlaymenu = document.querySelector('.overlay')
-const closex = document.querySelector('.fa-xmark')
-const dropdown = document.querySelector('.dropdown-content')
+//nav
+const navbar = document.getElementsByClassName("fa-bars")[0];
+const overlaymenu = document.querySelector(".overlay");
+const closex = document.querySelector(".fa-xmark");
+const dropdown = document.querySelector(".dropdown-content");
 
-
-navbar.addEventListener('click', openOverlay)
-closex.addEventListener('click', closeeOverlay)
+navbar.addEventListener("click", openOverlay);
+closex.addEventListener("click", closeeOverlay);
 
 function openOverlay() {
+  overlaymenu.classList.add("active");
 
-    overlaymenu.classList.add('active')
-
-    dropdown.classList.remove('dropdown-content')
+  dropdown.classList.remove("dropdown-content");
 }
 
-
 function closeeOverlay() {
-    overlaymenu.classList.remove('active')
+  overlaymenu.classList.remove("active");
 
-    dropdown.classList.add('dropdown-content')
-
-
+  dropdown.classList.add("dropdown-content");
 }
 
 //swiper
 var swiper = new Swiper(".mySwiper", {
-    pagination: {
-        el: ".swiper-pagination",
-    },
+  pagination: {
+    el: ".swiper-pagination",
+  },
 });
-
