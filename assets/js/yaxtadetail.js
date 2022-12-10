ef15895bd93f94621f3e655d422aed03b2d17862
unknown <chuseynov538@gmail.com>
2022-12-10 15:24:50 +0400

    yaxta detail page elave olundu

---
+++ assets/js/yaxtadetail.js
@@ -0,0 +1,36 @@
+
+const allHoverImages = document.querySelectorAll('.hover-container div img');
+const imgContainer = document.querySelector('.img-container');
+
+window.addEventListener('DOMContentLoaded', () => {
+    allHoverImages[0].parentElement.classList.add('active');
+});
+
+allHoverImages.forEach((image) => {
+    image.addEventListener('click', () =>{
+        imgContainer.querySelector('img').src = image.src;
+        resetActiveImg();
+        // image.parentElement.classList.add('active');
+    });
+});
+
+//nav
+const navbar = document.getElementsByClassName("fa-bars")[0];
+const overlaymenu = document.querySelector(".overlay");
+const closex = document.querySelector(".fa-xmark");
+const dropdown = document.querySelector(".dropdown-content");
+
+navbar.addEventListener("click", openOverlay);
+closex.addEventListener("click", closeeOverlay);
+
+function openOverlay() {
+  overlaymenu.classList.add("active");
+
+  dropdown.classList.remove("dropdown-content");
+}
+
+function closeeOverlay() {
+  overlaymenu.classList.remove("active");
+
+  dropdown.classList.add("dropdown-content");
+}
