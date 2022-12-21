051a39912268313346361380f4a939a79f68becc
unknown <chuseynov538@gmail.com>
2022-12-21 20:13:32 +0400

    duzelish edildi

--- assets/js/rentcarpage.js
+++ assets/js/rentcarpage.js
@@ -11,3 +11,72 @@ closepopup.addEventListener("click", function (e) {
   e.preventDefault();
   popup.classList.remove("active");
 });
+
+
+
+// nav
+
+const esasSehife = document.getElementsByClassName("esasSehife")[0]
+const yaxtaSehife = document.getElementsByClassName("yaxtaSehife")[0]
+const toyMashini = document.getElementsByClassName("toyMashini")[0]
+const miniVan = document.getElementsByClassName("miniVan")[0]
+const sedann = document.getElementsByClassName("sedann")[0]
+const elaqeee = document.getElementsByClassName("elaqeee")[0]
+const van = document.querySelector('.nav')
+
+esasSehife.addEventListener('click',ishlediEsas)
+yaxtaSehife.addEventListener('click',ishlediYaxta)
+toyMashini.addEventListener('click',ishlediToy)
+miniVan.addEventListener('click',ishlediVita)
+sedann.addEventListener('click',ishlediSedan)
+elaqeee.addEventListener('click',ishlediElaqe)
+function ishlediEsas(){
+  van.classList.add('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnn')
+  van.classList.remove('vannnnnn')
+  
+
+}
+function ishlediYaxta(){
+  van.classList.add('vann')
+  van.classList.remove('van')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnn')
+  van.classList.remove('vannnnnn')
+}
+function ishlediToy(){
+  van.classList.add('vannn')
+  van.classList.remove('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnn')
+  van.classList.remove('vannnnnn')
+}
+function ishlediVita(){
+  van.classList.add('vannnn')
+  van.classList.remove('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnnn')
+  van.classList.remove('vannnnnn')
+}
+function ishlediSedan(){
+  van.classList.add('vannnnn')
+  van.classList.remove('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnnn')
+}
+function ishlediElaqe(){
+  van.classList.add('vannnnnn')
+  van.classList.remove('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnn')
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,78 @@ closex.addEventListener("click", closeeOverlay);
 function openOverlay() {
   overlaymenu.classList.add("active");
 
-  dropdown.classList.remove("dropdown-content");
 }
 
 function closeeOverlay() {
   overlaymenu.classList.remove("active");
 
-  dropdown.classList.add("dropdown-content");
+}
+
+// nav
+
+const esasSehife = document.getElementsByClassName("esasSehife")[0]
+const yaxtaSehife = document.getElementsByClassName("yaxtaSehife")[0]
+const toyMashini = document.getElementsByClassName("toyMashini")[0]
+const miniVan = document.getElementsByClassName("miniVan")[0]
+const sedann = document.getElementsByClassName("sedann")[0]
+const elaqeee = document.getElementsByClassName("elaqeee")[0]
+const van = document.querySelector('.nav')
+
+esasSehife.addEventListener('click',ishlediEsas)
+yaxtaSehife.addEventListener('click',ishlediYaxta)
+toyMashini.addEventListener('click',ishlediToy)
+miniVan.addEventListener('click',ishlediVita)
+sedann.addEventListener('click',ishlediSedan)
+elaqeee.addEventListener('click',ishlediElaqe)
+function ishlediEsas(){
+  van.classList.add('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnn')
+  van.classList.remove('vannnnnn')
+  
+
+}
+function ishlediYaxta(){
+  van.classList.add('vann')
+  van.classList.remove('van')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnn')
+  van.classList.remove('vannnnnn')
+}
+function ishlediToy(){
+  van.classList.add('vannn')
+  van.classList.remove('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnn')
+  van.classList.remove('vannnnnn')
+}
+function ishlediVita(){
+  van.classList.add('vannnn')
+  van.classList.remove('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnnn')
+  van.classList.remove('vannnnnn')
+}
+function ishlediSedan(){
+  van.classList.add('vannnnn')
+  van.classList.remove('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnnn')
+}
+function ishlediElaqe(){
+  van.classList.add('vannnnnn')
+  van.classList.remove('van')
+  van.classList.remove('vann')
+  van.classList.remove('vannn')
+  van.classList.remove('vannnn')
+  van.classList.remove('vannnnn')
 }
 
 //swiper
