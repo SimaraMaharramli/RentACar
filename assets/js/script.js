ee207b4f11352c07c43ce747d470ed0b5a61d44e
unknown <chuseynov538@gmail.com>
2022-12-29 23:44:20 +0400

    cart ve overlay hazirdir

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,6 @@
 const navbar = document.getElementsByClassName("fa-bars")[0];
 const overlaymenu = document.querySelector(".overlay");
 const closex = document.querySelector(".fa-xmark");
-const dropdown = document.querySelector(".dropdown-content");
 
 navbar.addEventListener("click", openOverlay);
 closex.addEventListener("click", closeeOverlay);
