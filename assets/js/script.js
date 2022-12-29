ac9d66ef2d407e5bf5810e704a751ba1c1879036
zeyno12 <zeynabbagirova1@gmail.com>
2022-12-29 23:58:43 +0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,30 +144,10 @@ function ishlediElaqe(){
 
 }
 
-//swiper
-var swiper = new Swiper(".mySwiper", {
-  pagination: {
-    el: ".swiper-pagination",
-  },
-});
+// //swiper
+// var swiper = new Swiper(".mySwiper", {
+//   pagination: {
+//     el: ".swiper-pagination",
+//   },
+// });
 
-//acc
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-   
-    var panel = this.nextElementSibling;
-    
-
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-
-    } else {
-      panel.style.display = "block";  
-
-    }
-  });
-}
