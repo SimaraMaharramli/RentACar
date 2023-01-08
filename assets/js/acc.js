f8ca48a7410875f37b7886e085fb32d09811eb21
zeyno12 <zeynabbagirova1@gmail.com>
2023-01-08 22:06:21 +0400

    finished 2 pages

--- assets/js/acc.js
+++ assets/js/acc.js
@@ -3,7 +3,8 @@ var acc = document.getElementsByClassName("accordion");
 var i;
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
+  acc[i].addEventListener("click", function(e) {
+    e.preventDefault()
     this.classList.toggle("active");
    
     var panel = this.nextElementSibling;
